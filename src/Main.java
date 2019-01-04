c977926f325bac375e96b464bdf0d001bc656866
Alex <fuckyobitch@Alexs-MacBook-Pro-2.local>
2019-01-04 13:36:05 -0500

    fully functional, add, edit, delete names and phone numbers via terminal

---
+++ src/Main.java
@@ -0,0 +1,149 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("5179302749");
+
+    public static void main(String[] args) {
+
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while(!quit) {
+
+            System.out.println("\nEnter selection:  (Press 6 for Main Menu)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("\nShutting down...");
+                    quit = true;
+                    break;
+
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+
+                case 2:
+                    addNewContact();
+                    break;
+
+                case 3:
+                    updateContact();
+                    break;
+
+                case 4:
+                    removeContact();
+                    break;
+
+                case 5:
+                    queryContact();
+                    break;
+
+                case 6:
+                    printActions();
+                    break;
+
+            }
+        }
+    }
+
+    private static void addNewContact() {
+        System.out.println("Enter new contact name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phone = scanner.nextLine();
+        Contact newContact = Contact.createContact(name, phone);
+
+        if(mobilePhone.addNewContact(newContact)) {
+            System.out.println("New contact added: name = " + name + ", phone number = " + phone);
+        } else {
+            System.out.println("Can't add, " + name + " already on file");
+        }
+    }
+
+
+
+    private static void updateContact() {
+        System.out.println("Enter existing contact name: ");
+        String name = scanner.nextLine();
+        Contact existingContactRecord = mobilePhone.queryContact(name);
+
+        if(existingContactRecord == null) {
+            System.out.println("Contact not found");
+            return;
+        }
+
+        System.out.println("Enter new contact name: ");
+        String newName = scanner.nextLine();
+        System.out.println("Enter new contact phone number: ");
+        String newNumber = scanner.nextLine();
+
+        Contact newContact = Contact.createContact(newName, newNumber);
+
+        if(mobilePhone.updateContact(existingContactRecord, newContact)) {
+            System.out.println("Successfully updated contact");
+        } else {
+            System.out.println("Error updating contact");
+        }
+    }
+
+
+
+    private static void removeContact() {
+        System.out.println("Enter existing contact name: ");
+        String name = scanner.nextLine();
+        Contact existingContactRecord = mobilePhone.queryContact(name);
+
+        if (existingContactRecord == null) {
+            System.out.println("Contact not found");
+            return;
+        }
+
+        if(mobilePhone.removeContact(existingContactRecord)) {
+            System.out.println("Successfully deleted");
+        } else {
+            System.out.println("Error deleting contact");
+        }
+    }
+
+
+
+    private static void queryContact() {
+        System.out.println("Enter existing contact name: ");
+        String name = scanner.nextLine();
+        Contact existingContactRecord = mobilePhone.queryContact(name);
+
+        if (existingContactRecord == null) {
+            System.out.println("Contact not found");
+            return;
+        }
+
+        System.out.println("Name: " + existingContactRecord.getName() + " phone number is: " + existingContactRecord.getPhoneNumber());
+    }
+
+
+
+
+
+    private static void startPhone() {
+        System.out.println("Starting phone...");
+    }
+
+    private static void printActions() {
+        System.out.println("\nMake a selection: \npress");
+        System.out.println("0  -  shutdown\n" +
+                "1  -  display contacts\n" +
+                "2  -  add a new contact\n" +
+                "3  -  update an existing contact\n" +
+                "4  -  remove a contact\n" +
+                "5  -  query if contact exists\n" +
+                "6  -  display list of actions");
+        System.out.println("Choose an action:  ");
+    }
+
+
+
+}
